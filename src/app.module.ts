ddd2b6927f0eca64e2707ccdec25f657e3d45e7a
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2023-01-03 14:18:07 -0300

    feat(app): include environment service and logger to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,8 @@
-import { Module } from '@nestjs/common';
+import { EnvironmentModule } from '@core/environment';
+import { Logger, Module } from '@nestjs/common';
 
-@Module({})
+@Module({
+  imports: [EnvironmentModule],
+  providers: [Logger],
+})
 export class AppModule {}
