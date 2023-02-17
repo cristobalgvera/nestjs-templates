9e2b8821c8b9cd785e5b453e7a8b23c325a56b93
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2023-02-17 10:50:35 -0300

    perf(environment): improve schema tests creation

--- src/core/environment/validation/environment.schema.spec.ts
+++ src/core/environment/validation/environment.schema.spec.ts
@@ -9,54 +9,59 @@ describe('EnvironmentSchema', () => {
     IP_WHITELIST_EXAMPLE: ['127.0.0.1'],
   };
 
-  it.each([
-    { ...validEnvironment },
-    { ...validEnvironment, NODE_ENV: 'production' },
-    { ...validEnvironment, NODE_ENV: 'test' },
-    { ...validEnvironment, NODE_ENV: undefined },
-    { ...validEnvironment, PORT: undefined },
-    { ...validEnvironment, ENABLE_SWAGGER: undefined },
-    { ...validEnvironment, ENABLE_SWAGGER: false },
-    { ...validEnvironment, IP_WHITELIST: [] },
-    {
-      ...validEnvironment,
-      IP_WHITELIST_EXAMPLE: [
-        ...validEnvironment.IP_WHITELIST_EXAMPLE,
-        '10.0.0.2',
-      ],
-    },
-  ])('should properly validate %s', () => {
-    const validation = environmentSchema.validate(validEnvironment);
+  describe('when environment is valid', () => {
+    it.each<Partial<Environment>>([
+      { ...validEnvironment },
+      { NODE_ENV: 'production' },
+      { NODE_ENV: 'test' },
+      { NODE_ENV: undefined },
+      { PORT: undefined },
+      { ENABLE_SWAGGER: undefined },
+      { ENABLE_SWAGGER: false },
+      { IP_WHITELIST_EXAMPLE: [] },
+      {
+        IP_WHITELIST_EXAMPLE: [
+          ...validEnvironment.IP_WHITELIST_EXAMPLE,
+          '10.0.0.2',
+        ],
+      },
+    ])(
+      'should properly validate if environment has %s',
+      (partialEnvironment) => {
+        const environment = {
+          ...validEnvironment,
+          ...partialEnvironment,
+        };
 
-    expect(validation.error).toBeUndefined();
-    expect(validation.value).toEqual(validEnvironment);
+        const validation = environmentSchema.validate(environment);
+
+        expect(validation.error).toBeUndefined();
+      },
+    );
   });
 
-  it.each([
-    { ...validEnvironment, NODE_ENV: 'invalid' },
-    { ...validEnvironment, PORT: 'invalid' },
-    { ...validEnvironment, ENABLE_SWAGGER: 'invalid' },
-    { ...validEnvironment, IP_WHITELIST_EXAMPLE: undefined },
-    { ...validEnvironment, IP_WHITELIST_EXAMPLE: ['invalid-ip'] },
-    {
-      ...validEnvironment,
-      IP_WHITELIST_EXAMPLE: [
-        ...validEnvironment.IP_WHITELIST_EXAMPLE,
-        'invalid-ip',
-      ],
-    },
-    { ...validEnvironment, PUB_SUB_PROJECT_ID: undefined },
-    { ...validEnvironment, PUB_SUB_PROJECT_ID: 1234 },
-    { ...validEnvironment, PUB_SUB_API_ENDPOINT: undefined },
-    { ...validEnvironment, PUB_SUB_API_ENDPOINT: 'invalid-endpoint' },
-    { ...validEnvironment, PUB_SUB_TOPIC: undefined },
-    { ...validEnvironment, PUB_SUB_TOPIC: 123 },
-    { ...validEnvironment, PUB_SUB_PATTERN: undefined },
-    { ...validEnvironment, PUB_SUB_PATTERN: 123 },
-    {},
-  ])('should invalidate %s', (invalidValue) => {
-    const validation = environmentSchema.validate(invalidValue);
+  describe('when environment is invalid', () => {
+    it.each<Partial<Record<keyof Environment, unknown>>>([
+      { NODE_ENV: 'invalid' },
+      { PORT: 'invalid' },
+      { ENABLE_SWAGGER: 'invalid' },
+      { IP_WHITELIST_EXAMPLE: undefined },
+      { IP_WHITELIST_EXAMPLE: ['invalid-ip'] },
+      {
+        IP_WHITELIST_EXAMPLE: [
+          ...validEnvironment.IP_WHITELIST_EXAMPLE,
+          'invalid-ip',
+        ],
+      },
+    ])('should invalidate if environment has %s', (partialEnvironment) => {
+      const environment = {
+        ...validEnvironment,
+        ...partialEnvironment,
+      };
 
-    expect(validation.error).toBeDefined();
+      const validation = environmentSchema.validate(environment);
+
+      expect(validation.error).toBeDefined();
+    });
   });
 });
