55c09443344b9a295d2703073aa3a035d7d9632a
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2023-05-25 12:36:53 -0400

    feat(app): include the bank standard exception filter as default error handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { EnvironmentModule } from '@core/environment';
 import { Logger, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { BankStandardExceptionModule } from '@core/exception-filters';
 
 @Module({
-  imports: [EnvironmentModule],
+  imports: [EnvironmentModule, BankStandardExceptionModule],
   providers: [Logger],
+  controllers: [AppController],
 })
 export class AppModule {}
