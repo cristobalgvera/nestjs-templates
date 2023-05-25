80415861b8c7bd08e719b79011c1331898ded0b8
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2023-05-25 15:59:41 -0400

    feat(app): include bank standard success interceptor as global one

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { EnvironmentModule } from '@core/environment';
-import { Logger, Module } from '@nestjs/common';
 import { BankStandardExceptionModule } from '@core/exception-filters';
+import { BankStandardSuccessModule } from '@core/interceptors';
+import { Logger, Module } from '@nestjs/common';
 
 @Module({
-  imports: [EnvironmentModule, BankStandardExceptionModule],
+  imports: [
+    EnvironmentModule,
+    BankStandardExceptionModule,
+    BankStandardSuccessModule.forRoot({
+      // TODO: Add proper service domain name code
+      serviceDomainNameCode: 'SERVICE_DOMAIN_NAME_CODE',
+    }),
+  ],
   providers: [Logger],
 })
 export class AppModule {}
