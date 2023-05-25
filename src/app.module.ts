42c9a6c893801a94029f8952dafdcf3ca14ddb09
Cristóbal Gajardo Vera <cgajardoo@bancoripley.com>
2023-05-25 15:18:40 -0400

    fix(app): remove bad app controller reference

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { EnvironmentModule } from '@core/environment';
 import { Logger, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { BankStandardExceptionModule } from '@core/exception-filters';
 
 @Module({
   imports: [EnvironmentModule, BankStandardExceptionModule],
   providers: [Logger],
-  controllers: [AppController],
 })
 export class AppModule {}
