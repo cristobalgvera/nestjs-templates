570f76c0d26174df5bf4b9da36f7873def3b9165
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-06-05 16:33:36 -0400

    feat(firestore): include firestore module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
-import { EnvironmentModule } from '@core/environment';
+import { EnvironmentModule, EnvironmentService } from '@core/environment';
 import { Logger, Module } from '@nestjs/common';
+import { FirestoreModule } from '@features/firestore';
 
 @Module({
-  imports: [EnvironmentModule],
+  imports: [
+    EnvironmentModule,
+    FirestoreModule.forRoot({
+      imports: [EnvironmentModule],
+      useFactory: (environmentService: EnvironmentService) => ({
+        projectId: environmentService.get('FIRESTORE_PROJECT_ID'),
+      }),
+      inject: [EnvironmentService],
+    }),
+  ],
   providers: [{ provide: Logger, useValue: new Logger(AppModule.name) }],
 })
 export class AppModule {}
