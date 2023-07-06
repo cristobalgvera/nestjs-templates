09446188123b2c793133b26eb19b7c063cfbd425
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-07-06 12:30:13 -0400

    refactor(redis): remove unnecesary environment module import

--- src/features/redis/redis.module.ts
+++ src/features/redis/redis.module.ts
@@ -1,10 +1,8 @@
-import { EnvironmentModule } from '@core/environment';
 import { Module } from '@nestjs/common';
 import { RedisProvider } from './redis.provider';
 import { RedisService } from './redis.service';
 
 @Module({
-  imports: [EnvironmentModule],
   providers: [RedisProvider.provide(), RedisService],
   exports: [RedisService],
 })
