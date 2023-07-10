2656a0da32b3f236bd6910b26b4924c81aed93c4
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-07-10 18:04:44 -0400

    refactor(redis): fix lint issues

--- src/features/redis/redis.provider.ts
+++ src/features/redis/redis.provider.ts
@@ -4,19 +4,17 @@ import { Redis } from 'ioredis';
 
 export const REDIS_CLIENT = 'REDIS_CLIENT';
 
-export class RedisProvider {
-  static provide(): FactoryProvider<Redis> {
-    return {
-      provide: REDIS_CLIENT,
-      inject: [EnvironmentService],
-      useFactory(environmentService: EnvironmentService) {
-        return new Redis({
-          lazyConnect: true,
-          host: environmentService.get('REDISHOST'),
-          port: environmentService.get('REDISPORT'),
-          password: environmentService.get('REDISAUTH'),
-        });
-      },
-    };
-  }
-}
+export const RedisProvider = {
+  provide: (): FactoryProvider<Redis> => ({
+    provide: REDIS_CLIENT,
+    inject: [EnvironmentService],
+    useFactory(environmentService: EnvironmentService) {
+      return new Redis({
+        lazyConnect: true,
+        host: environmentService.get('REDISHOST'),
+        port: environmentService.get('REDISPORT'),
+        password: environmentService.get('REDISAUTH'),
+      });
+    },
+  }),
+};
