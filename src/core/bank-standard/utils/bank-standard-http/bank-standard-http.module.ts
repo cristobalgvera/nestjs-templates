24867fb4568045149b754e86506fafb84483bead
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-07-14 15:17:17 -0400

    feat(bank-standard): create for feature method to provide the custom http module

--- src/core/bank-standard/utils/bank-standard-http/bank-standard-http.module.ts
+++ src/core/bank-standard/utils/bank-standard-http/bank-standard-http.module.ts
@@ -27,4 +27,34 @@ export class BankStandardHttpModule {
       exports: [HttpModule],
     };
   }
+
+  /**
+   * @description
+   *
+   * Make available the custom `HttpModule` inside a feature module
+   * that can not obtain it through the global scope.
+   *
+   * Commonly, you simply need to import the `BankStandardHttpModule` using
+   * the `BankStandardHttpModule#forRoot` method in the main module,
+   * but in some cases, like when you are using extensive usage of `DI`,
+   * you will need to import the `BankStandardHttpModule` in the feature module.
+   *
+   * ---
+   *
+   * ```typescript
+   * @Module({
+   *   imports: [BankStandardHttpModule.forFeature()],
+   *   providers: [{ provide: ABSTRACT_SERVICE_TOKEN, useClass: ConcreteService }],
+   *   exports: [ABSTRACT_SERVICE_TOKEN],
+   * })
+   * export class CustomModule {}
+   * ```
+   */
+  static forFeature(): DynamicModule {
+    return {
+      module: BankStandardHttpModule,
+      imports: [BankStandardHttpModule],
+      exports: [BankStandardHttpModule],
+    };
+  }
 }
